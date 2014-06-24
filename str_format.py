3923022bad065f02cb59dfe6ef57ef4a0494a64c
finico <finico.a@gmail.com>
2014-06-24 23:30:31 +0400

    str_format

---
+++ str_format.py
@@ -0,0 +1,9 @@
+age = 27
+name = 'Alexey'
+
+print('Возраст {0} - {1} лет.'.format(name, age))
+print('Почему {0} забавляется с этим Python?'.format(name))
+
+print('{0:.3}'.format(1/3))
+print('{0:_^11}'.format('hello'))
+print('{name} написал {book}'.format(name='Swaroop', book='A Byte Of Python'))
