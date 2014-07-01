7914fd7b7b14befc621395219684f485616c2952
a.gonchar <a.gonchar@protonservice.ru>
2014-07-01 17:37:35 +0400

    func_param

---
+++ func_param.py
@@ -0,0 +1,18 @@
+def print_max(a, b):
+    if a > b:
+        print(a, 'максимально')
+    elif a == b:
+        print(a, 'равно', b)
+    else:
+        print(b, 'максимально')
+
+print_max(3, 4)
+
+x = 5
+y = 7
+
+print_max(x, y)
+
+number = 2
+second_number = 2
+print_max(number, second_number)
