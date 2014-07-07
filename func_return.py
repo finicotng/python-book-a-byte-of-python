a5d902dca5fcf0e1e5e6cdfca02b693f3c9fc019
finico <finico.a@gmail.com>
2014-07-07 16:08:12 +0400

    func return

---
+++ func_return.py
@@ -0,0 +1,12 @@
+def maximum(x, y):
+    if x > y:
+        return x
+    elif x == y:
+        return 'числа равны'
+    else:
+        return y
+
+print(maximum(2, 3))
+
+#native
+print(max(22, 33))
