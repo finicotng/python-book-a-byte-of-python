45144fd0e31cecf3614da6a84dad8e65df40f35a
finico <finico.a@gmail.com>
2014-07-15 16:09:48 +0400

    backup ver 2

--- exercises/backup_ver2.py
+++ exercises/backup_ver2.py
@@ -8,7 +8,13 @@ source = ['a-byte-of-python']
 # чтобы не париться, архив кладем туда же, где исходники
 target_dir = source_dir
 
-target = target_dir + os.sep + time.strftime('%Y.%m.%d_%H-%M-%S') + '.zip'
+today = time.strftime('%H%M%S')
+
+if not os.path.exists(today):
+    os.mkdir(target_dir + today)  # создание папки, если ее нет
+print('папка создана')
+
+target = today + os.sep + time.strftime('%H%M%S') + '.zip'
 
 # команда архивирования
 zip_command = "cd {0} && zip -qr {1} {2}".format(source_dir, target, ' '.join(source))
